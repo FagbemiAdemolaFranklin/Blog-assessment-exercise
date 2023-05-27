e047d9ff6cf6f8c4f65f2cd73f9cf9e324c2d937
Franklin <fraklinademola4@gmail.com>
2023-05-27 13:10:18 +0200

    issue resolvement

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ async function main() {
     });
  
     app.get("/blogs", async (request, response) => {
-        let username = await request.session.passport.user.username;
+        const username = await request.session.passport.user.username;
         try{
             
             await blogs.find({}).then((foundBlogs) => {
@@ -201,8 +201,8 @@ async function main() {
     });
 
     app.post("/blogs", async (request, response) => {
-        let username = await request.session.passport.user.username;
-        let fullname = await request.session.passport.user.fullname;
+        const username = await request.session.passport.user.username;
+        const fullname = await request.session.passport.user.fullname;
         var {comment, submit, title, content, addcomment, like} = request.body
         if(addcomment) {
             var commentDetails = {
